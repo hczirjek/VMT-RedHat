40b8acc8fc9f5e75d1dd1750214388eb20a14abd
hczirjek <czirjekhunor9@gmail.com>
2019-01-13 18:56:31 +0200

    update

--- VisualMoodTracker/VisualMoodTracker/Program.cs
+++ VisualMoodTracker/VisualMoodTracker/Program.cs
@@ -1,25 +1,18 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
 
 namespace WebApplicationNETCore
 {
     public class Program
     {
-        private static string url;
-
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder().AddEnvironmentVariables("").Build();
-            url = config["ASPNETCORE_URLS"] ?? "http://*:8080";
             BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls(url)
                 .Build();
     }
 }
